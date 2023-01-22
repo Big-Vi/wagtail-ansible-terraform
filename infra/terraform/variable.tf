0df68abf2c8b12d19c895ba021875cfc4bc7affe
big-vi <vignesh.murugan@hotmail.com>
2023-01-22 18:57:55 +1300

    rds with secret manager

--- infra/terraform/variable.tf
+++ infra/terraform/variable.tf
@@ -9,6 +9,11 @@ variable "region" {
   default     = "ap-southeast-2"
 }
 
+variable "public_subnets" {
+  description = "VPC's public subnets list"
+  default = ["192.168.0.0/20", "192.168.16.0/20"]
+}
+
 # variable "profile" {
 #   description = "profile for deploy"
 #   default     = "default"
